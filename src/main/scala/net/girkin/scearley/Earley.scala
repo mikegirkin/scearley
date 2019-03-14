3778a9cdd3fe1db4c4fe2e5313905493f10585b0
Mikhail Girkin <mikegirkin@gmail.com>
2019-03-14 10:59:43 +0000

    predictor implementation

--- src/main/scala/net/girkin/scearley/Earley.scala
+++ src/main/scala/net/girkin/scearley/Earley.scala
@@ -5,27 +5,26 @@ import net.girkin.scearley.test.Grammar
 import scala.collection._
 import scala.collection.mutable.ArrayBuffer
 
-case class EarleyRecord(ruleLeft: NonTerminal, ruleExpansion: IndexedSeq[Symbol], dotPosition: Int, startPosition: Int)
+case class EarleyRecord(
+  ruleLeft: NonTerminal,
+  ruleExpansion: IndexedSeq[Symbol],
+  dotPosition: Int,
+  startPosition: Int
+) {
+  def currentSymbol: Symbol = ruleExpansion(dotPosition)
+
+  def finished: Boolean = {
+    dotPosition == ruleExpansion.length
+  }
+}
 
 class Earley {
   type TInToken = String
 
-  private def nextElementOf(ruleState: EarleyRecord): Symbol = {
-    ruleState.ruleExpansion(ruleState.dotPosition)
-  }
-
-  private def isNextElementTerminal(ruleState: EarleyRecord): Boolean = {
-    nextElementOf(ruleState).isTerminal
-  }
-
-  private def finihed(ruleState: EarleyRecord): Boolean = {
-    ruleState.dotPosition == ruleState.ruleExpansion.length
-  }
-
-  private def scanner(ruleState: EarleyRecord, symbol: Terminal, index:Int, word: TInToken): Option[(Int, EarleyRecord)] = {
+  private def scanner(ruleState: EarleyRecord, symbol: Terminal, currentSymbolIndex:Int, word: TInToken): Option[(Int, EarleyRecord)] = {
     symbol.matcher.tryMatch(word).map { _ =>
       (
-        index + 1,
+        currentSymbolIndex + 1,
         ruleState.copy(
           dotPosition = ruleState.dotPosition + 1,
         )
@@ -33,34 +32,43 @@ class Earley {
     }
   }
 
-  private def predictor() = ???
+  private def predictor(table: Map[Int, IndexedSeq[EarleyRecord]], grammar: Grammar, ruleState: EarleyRecord, currentSymbolIndex: Int): Seq[(Int, EarleyRecord)] = {
+    val expandingSymbol = ruleState.currentSymbol
+    val applicableRules: IndexedSeq[(NonTerminal, IndexedSeq[Symbol])] =
+      grammar.filter(r => r.left == expandingSymbol)
+        .flatMap(r => r.expansion.map(ruleLine => r.left -> ruleLine))
 
-  private def completer(table: Map[Int, IndexedSeq[EarleyRecord]], completingRuleState: EarleyRecord, currentIndex: Int):Seq[(Int, EarleyRecord)] = {
+    applicableRules.map { case (symbolLeft, ruleExpansion) =>
+      currentSymbolIndex -> EarleyRecord(symbolLeft, ruleExpansion, 0, currentSymbolIndex)
+    }
+  }
+
+  private def completer(table: Map[Int, IndexedSeq[EarleyRecord]], completingRuleState: EarleyRecord, currentSymbolIndex: Int): Seq[(Int, EarleyRecord)] = {
     for {
       ruleState <- table(completingRuleState.startPosition).filter(rule => rule.ruleExpansion(rule.dotPosition) == completingRuleState.ruleLeft)
     } yield {
-      currentIndex -> ruleState.copy(
+      currentSymbolIndex -> ruleState.copy(
         dotPosition = ruleState.dotPosition + 1
       )
     }
   }
 
 
+  def addToTable(table: mutable.Map[Int, mutable.ArrayBuffer[EarleyRecord]], items: Seq[(Int, EarleyRecord)]): Unit = {
+    for {
+      (index, ruleState) <- items
+    } {
+      if (table.contains(index)) table(index).append(ruleState)
+      else {
+        table(index) = new mutable.ArrayBuffer[EarleyRecord]
+        table(index).append(ruleState)
+      }
+    }
+  }
+
   def parse(grammar: Grammar, content: Seq[TInToken]): Boolean = {
 
     val table: mutable.Map[Int, mutable.ArrayBuffer[EarleyRecord]] = mutable.Map[Int, mutable.ArrayBuffer[EarleyRecord]]()
-
-    def addToTable(items: Seq[(Int, EarleyRecord)]): Unit = {
-      for {
-        (index, ruleState) <- items
-      } {
-        if (table.contains(index)) table(index).append(ruleState)
-        else {
-          table(index) = new mutable.ArrayBuffer[EarleyRecord]
-          table(index).append(ruleState)
-        }
-      }
-    }
 
     val start = EarleyRecord(grammar(0).left, grammar(0).expansion(0), 0, 0)
     table(0) = new mutable.ArrayBuffer()
@@ -72,17 +80,20 @@ class Earley {
       var currentPosition = 0
       while(currentPosition < table.getOrElse(index, ArrayBuffer.empty).length) {
         val ruleState = table.getOrElse(index, ArrayBuffer.empty)(currentPosition)
-        if(!finihed(ruleState)) {
-          nextElementOf(ruleState) match {
+        if(!ruleState.finished) {
+          ruleState.currentSymbol match {
             case symbol @ Terminal(_) => {
               val newRules = scanner(ruleState, symbol, index, word)
-              addToTable(newRules.toSeq)
+              addToTable(table, newRules.toSeq)
             }
-            case s @ NonTerminal(_) => ???
+            case s @ NonTerminal(_) => {
+              val newRules = predictor(table, grammar, ruleState, index)
+              addToTable(table, newRules)
+            }
           }
         } else {
           val newRules = completer(table, ruleState, index)
-          addToTable(newRules)
+          addToTable(table, newRules)
         }
         currentPosition += 1
       }
--- src/test/scala/net/girkin/scearley/EarleySpec.scala
+++ src/test/scala/net/girkin/scearley/EarleySpec.scala
@@ -28,7 +28,7 @@ class EarleySpec extends FunSpec with Matchers{
     }
   }
 
-  describe("parsing with grammar 1 rule of 3 non-terminals") {
+  describe("parsing with grammar 1 rule of 3 terminals") {
     val tests = Seq(
       Seq("1" , "+", "1") -> true,
       Seq("11", "12") -> false,
@@ -38,6 +38,37 @@ class EarleySpec extends FunSpec with Matchers{
     val grammar: Grammar = Vector[Rule](
       Rule(NonTerminal("MAIN"), Vector(
         Vector(integer, str("+"), integer)))
+    )
+
+    for {
+      (input, expected) <- tests
+    } {
+      it(s"parses correctly ${input}") {
+        earley.parse(grammar, input) shouldBe expected
+      }
+    }
+  }
+
+  describe("parsing using grammar [ EXPR :- int OP int ]") {
+    // EXPR :- int OP int
+    // OP :- '+'
+   //      | '-'
+    val grammar: Grammar = Vector[Rule](
+      Rule(NonTerminal("EXPR"), Vector(
+        Vector(integer, NonTerminal("OP"), integer)
+      )),
+      Rule(NonTerminal("OP"), Vector(
+        Vector(str("-")),
+        Vector(str("+"))
+      ))
+    )
+
+    val tests = Seq(
+      Seq("1", "+", "1") -> true,
+      Seq("155", "-", "12") -> true,
+      Seq("-", "12314", "+", "1234") -> false,
+      Seq("155", "-", "12", "123") -> false,
+      Seq("155", "asdj", "12", "123") -> false,
     )
 
     for {
